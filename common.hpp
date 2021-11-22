9580941abb7bce8bc05c73faf849754d8c6b709f
yeting <ye.ting@h3c.com>
2021-11-22 02:21:23 +0000

    first commit

---
+++ common.hpp
@@ -0,0 +1,20 @@
+#ifndef __COMMON_LEARN_HEADER__
+#define __COMMON_LEARN_HEADER__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+
+#endif // !__COMMON_LEARN_HEADER__
